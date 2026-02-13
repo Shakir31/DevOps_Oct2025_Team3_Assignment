5c0b6e48cfe5b1061af2d0cde4c16b9ef557f2f6
Alexchua <S10259411@connect.np.edu.sg>
2026-02-13 12:53:32 +0800

    Step 4-5: Fix setup.js and adminController.test.js assertion

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,8 +1,1 @@
-
 require('dotenv').config();
-
-const nodeFetch = require('node-fetch');
-global.fetch = nodeFetch;
-global.Headers = nodeFetch.Headers;
-global.Request = nodeFetch.Request;
-global.Response = nodeFetch.Response;
